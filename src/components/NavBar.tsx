d8f8ac771449293fa798686d15a68e6548dabb85
NassimSleem <nassimsleem5@gmail.com>
2023-06-17 15:35:42 +0300

    Last changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,47 +1,91 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../App";
 
 function NavBar() {
+  const searchModal = useRef<any>(null);
   const { state, dispatch } = useContext(UserContext);
+  const [search, setSearch] = useState("");
+  const [users, setUsers] = useState<any>([]);
   const navigation = useNavigate();
+
+  useEffect(() => {
+    M.Modal.init(searchModal.current);
+  }, []);
 
   const renderList = () => {
     if (state) {
       return [
-        <li>
+        <li key="4">
+          <i
+            data-target="modal1"
+            className="large material-icons modal-trigger"
+            style={{
+              color: "black",
+              cursor: "pointer",
+              marginRight: " 10px",
+            }}
+          >
+            search
+          </i>
+        </li>,
+        <li key="1">
           <Link to="/profile">Profile</Link>
         </li>,
 
-        <li>
+        <li key="2">
           <Link to="/create">Create Post</Link>
         </li>,
 
-        <li>
+        <li key="3">
           <Link to="/followedPosts">Following Page</Link>
         </li>,
-        <button
-          className="btn #c62828 red darken-3"
-          onClick={() => {
-            localStorage.clear();
-            dispatch({ type: "CLEAR" });
-            navigation("/signin");
-          }}
-        >
-          Logout
-        </button>,
+
+        <li key="5">
+          <button
+            className="btn #c62828 red darken-3"
+            onClick={() => {
+              localStorage.clear();
+              dispatch({ type: "CLEAR" });
+              navigation("/signin");
+            }}
+          >
+            Logout
+          </button>
+          ,
+        </li>,
       ];
     } else {
       return [
-        <li>
+        <li key="1">
           <Link to="/SignIn">SignIn</Link>
         </li>,
-        <li>
+        <li key="2">
           <Link to="/signup">Sign Up</Link>
         </li>,
       ];
     }
   };
+
+  const fetchUsers = (value: any) => {
+    setSearch(value);
+    fetch("/search-users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        query: value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("res", res);
+        setUsers(res);
+      });
+  };
+
   return (
     <nav>
       <div className="nav-wrapper white">
@@ -51,6 +95,54 @@ function NavBar() {
         <ul id="nav-mobile" className="right">
           {renderList()}
         </ul>
+      </div>
+      {/* Modal Structure */}
+      <div id="modal1" className="modal" ref={searchModal}>
+        <div
+          className="modal-content"
+          style={{
+            color: "black",
+          }}
+        >
+          <b>Search</b>
+          <input
+            type="text"
+            placeholder="search for users"
+            value={search}
+            onChange={(value) => fetchUsers(value.target.value)}
+          />
+          <ul className="collection">
+            {users.map((item: any) => {
+              return (
+                <li className="collection-item" key={item._id}>
+                  <Link
+                    to={
+                      item._id === state._id
+                        ? "/profile"
+                        : "/profile/" + item._id
+                    }
+                    onClick={() => {
+                      M.Modal.getInstance(searchModal.current).close();
+                    }}
+                  >
+                    <b>{item.name}</b>: {item.email}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="modal-footer">
+          <button
+            className="modal-close waves-effect waves-green btn-flat"
+            onClick={() => {
+              setSearch("");
+              setUsers([]);
+            }}
+          >
+            Close
+          </button>
+        </div>
       </div>
     </nav>
   );
